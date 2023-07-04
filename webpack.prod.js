fed3937aefbab68a90f9a8442e9b75e5c926c572
David Huynh <david.huynh@i-on.net>
2023-07-04 15:31:21 +0700

    connect api

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,10 +11,12 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "product-detail",
+      name: "detail",
       filename: "remoteEntry.js",
       exposes: {
         "./App": "./src/bootstrap",
+        "./ProductDetail": "./src/components/ProductDetail",
+        "./Cart": "./src/components/Cart",
       },
       shared: packageJson.dependencies,
     }),
