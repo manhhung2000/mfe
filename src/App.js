359d5b71757eebbf808568c99f32be3d96a0a978
David Huynh <david.huynh@i-on.net>
2023-07-04 15:59:12 +0700

    connect api 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from "./components/ProductDetail";
 import Cart from "./components/Cart";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <div className="wrapper">
         <Routes>
+          <Route path="/" Component={Home} />
           <Route path="/product/:id" Component={ProductDetail} />
           <Route path="/cart" Component={Cart} />
         </Routes>
